03e50a9afbf671a6513debe7f5d730e2e1dd1844
mzimmerm <mzimmerm@NBNLI00703.nli.local>
2016-07-20 13:12:35 +0200

    login / logout functionality, navigation, ngroute
    spring security is missing
    
    Change-Id: I91525ec73d6b3d3a85ad475aed2fe2ebb5f3d040

--- src/main/java/com/nextlevel/example/angularjs15/RegisterController.java
+++ src/main/java/com/nextlevel/example/angularjs15/RegisterController.java
@@ -1,14 +1,19 @@
 package com.nextlevel.example.angularjs15;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegisterController {
+
+	private static Principal PRINCIPAL;
 
 	@RequestMapping("/resource")
 	public Map<String, Object> home() {
@@ -21,6 +26,36 @@ public class RegisterController {
 		model.put("id", UUID.randomUUID().toString());
 		model.put("content", "Hello World!!!");
 		return model;
+	}
+
+	@RequestMapping("/user")
+	public Principal user(@RequestHeader(value = "Authorization", defaultValue = "") String auth) {
+		if (PRINCIPAL != null) {
+			System.out.println("already logged in");
+			return PRINCIPAL;
+		}
+
+		if (StringUtils.isEmpty(auth)) {
+			System.out.println("authentication required");
+			return null;
+		} else {
+			PRINCIPAL = new Principal() {
+
+				@Override
+				public String getName() {
+					return auth;
+				}
+			};
+			System.out.println("logged in");
+			return PRINCIPAL;
+		}
+	}
+
+
+	@RequestMapping("/logout")
+	public void logout() {
+		System.out.println("logged out");
+		PRINCIPAL = null;
 	}
 
 }
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,11 +1,75 @@
-angular.module('hello', [])
-  .controller('home', function($http) {
-	  var self = this;
-	  $http.get('/resource').then(function(response) {
-		  self.greeting = response.data;
-	  }, function(reason) {
-	  	
-	  }, function(value) {
-	  	
-	  })
-})
+angular.module('hello', [ 'ngRoute' ])
+.config(
+		function($routeProvider, $httpProvider) {
+			$routeProvider.when('/', {
+				templateUrl : 'home.html',
+				controller : 'home',
+				controllerAs : 'controller'
+			}).when('/login', {
+				templateUrl : 'login.html',
+				controller : 'navigation',
+				controllerAs : 'controller'
+			}).otherwise('/');
+		})
+.controller('home', function($http) {
+	var self = this;
+	$http.get('/resource').then(function(response) {
+			self.greeting = response.data;
+		}, function(reason) {
+	
+		}, function(value) {
+	
+		})
+	})
+.controller(
+		'navigation',
+
+		function($rootScope, $http, $location) {
+
+			var self = this
+
+			var authenticate = function(credentials, callback) {
+
+				var headers = credentials ? {
+					authorization : "Basic "
+							+ btoa(credentials.username + ":"
+									+ credentials.password)
+				} : {};
+
+				$http.get('user', {
+					headers : headers
+				}).then(function(response) {
+					if (response.data.name) {
+						$rootScope.authenticated = true;
+					} else {
+						$rootScope.authenticated = false;
+					}
+					callback && callback();
+				}, function() {
+					$rootScope.authenticated = false;
+					callback && callback();
+				});
+
+			}
+
+			authenticate();
+			self.credentials = {};
+			self.login = function() {
+				authenticate(self.credentials, function() {
+					if ($rootScope.authenticated) {
+						$location.path("/");
+						self.error = false;
+					} else {
+						$location.path("/login");
+						self.error = true;
+					}
+				});
+			};
+			self.logout = function() {
+				  $http.get('logout', {}).finally(function() {
+				    self.authenticated = false;
+				    self.admin = false;
+				    $location.path("/login");
+				  });
+				}
+		});
